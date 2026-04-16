c55a39808e9a27ca40b9cb6f669f271e94fb753b
kirinpuzzo <kpuzzo@calpoly.edu>
2026-04-16 14:18:39 -0700

    Changed email

--- main.py
+++ main.py
@@ -9,3 +9,30 @@ def welcome_message(name:str) -> str:
 
 message = welcome_message("kpuzzo@calpoly.edu")
 print(message)
+
+
+def smallest(n: float, m: float) -> float:
+   if n < m:
+      return n  # For which calls below is this statement evaluated? For neither, this is never evaluated.
+   else:
+      return m
+
+
+first = smallest(3, 2)  # What is the value of first? 2
+second = smallest(2, 2)  # What is the value of second? Is this a reasonable result? Why or why not? 2, no because it does not consider if values are equal. There should be another statement saying if they are equal neither is smaller.
+print()
+
+
+def function2(a: int, b: int, c: int) -> int:
+   if a > b and a > c:
+      return a - b  # In general, when will a call to this function evaluate this statement? If a is the largest number
+   elif b > c:
+      return b + c  # In general, when will a call to this function evaluate this statement? if b is the largest number.
+   else:
+      return 2 * c  # In general, when will a call to this function evaluate this statement? If C is the largest number or two values are equal
+
+
+answer1 = function2(3, 2, 1)  # What is the value of answer1? 1
+answer2 = function2(2, 3, 1)  # What is the value of answer2? 4
+answer3 = function2(2, 1, 3)  # What is the value of answer3? 6
+print()
